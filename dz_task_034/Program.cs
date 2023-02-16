c86af469450861019e896fb259beb511676eb5df
Anton Kargapolov <igloooooook@gmail.com>
2023-02-16 18:52:22 +0500

    dz_task_034

---
+++ dz_task_034/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[ " + string.Join(", ", array) + "]");
+}
+
+int findCall(int[] array)
+{
+    int res = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            res++;
+        }
+
+    }
+    return res;
+}
+
+int[] myArray = GenerateArray(20, 100, 999);
+PrintArray(myArray);
+System.Console.WriteLine(findCall(myArray));
