ef61db946766c0c7aca60c178b2c5d166cd8dad4
Anton Kargapolov <igloooooook@gmail.com>
2023-02-16 15:43:38 +0500

    task 033

--- task_033/Program.cs
+++ task_033/Program.cs
@@ -1,3 +1,52 @@
 ﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // -3; массив [6, 7, 19, 345, -3] -> да
+
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[ " + string.Join(", ", array) + "]");
+}
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int num = InputNumber("Введите число ");
+
+bool findSame(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == num)
+        {
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+int[] myArray = GenerateArray(12, -9, 9);
+PrintArray(myArray);
+if(findSame(myArray,num))
+{
+    System.Console.WriteLine("Такой элемент есть");
+}
+else
+{
+    System.Console.WriteLine("Такого элемента нет");
+}
+
